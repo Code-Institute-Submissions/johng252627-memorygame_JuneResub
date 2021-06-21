50368463ba171f0f2de7fb329866a0ae8f4c930a
johng252627 <johngill252627@gmail.com>
2021-06-21 19:38:14 +0000

    Add jQuery for other ids

--- assets/script.js
+++ assets/script.js
@@ -3,3 +3,33 @@ $(document).ready(function(){
         $("#memorise1").text("Result");
     });
 });
+
+$(document).ready(function(){
+    $(".puzzle").click(function(){
+        $("#memorise2").text("Result");
+    });
+});
+
+$(document).ready(function(){
+    $(".puzzle").click(function(){
+        $("#memorise3").text("Result");
+    });
+});
+
+$(document).ready(function(){
+    $(".puzzle").click(function(){
+        $("#memorise4").text("Result");
+    });
+});
+
+$(document).ready(function(){
+    $(".puzzle").click(function(){
+        $("#memorise5").text("Result");
+    });
+});
+
+$(document).ready(function(){
+    $(".puzzle").click(function(){
+        $("#memorise6").text("Result");
+    });
+});
