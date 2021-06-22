92fc7132322f9ea4ff94e1faea85ae4d25288cca
johng252627 <johngill252627@gmail.com>
2021-06-22 16:52:46 +0000

    Change script.js to match other puzzles.

--- assets/script.js
+++ assets/script.js
@@ -9,31 +9,45 @@ $(document).ready(function(){
 
 $(document).ready(function(){
     $("#puzzle2").click(function(){
-        $("#memorise2").text("Result");
+        $("#memorise2").text("Result").delay(2000).queue(function(next){
+            $(this).text("Test");
+            next();
+        });
     });
 });
 
 $(document).ready(function(){
     $("#puzzle3").click(function(){
-        $("#memorise3").text("Result");
+        $("#memorise3").text("Result").delay(2000).queue(function(next){
+            $(this).text("Test");
+            next();
+        });
     });
 });
 
 $(document).ready(function(){
     $("#puzzle4").click(function(){
-        $("#memorise4").text("Result");
+        $("#memorise4").text("Result").delay(2000).queue(function(next){
+            $(this).text("Test");
+            next();
+        });
     });
 });
 
 $(document).ready(function(){
     $("#puzzle5").click(function(){
-        $("#memorise5").text("Result");
+        $("#memorise5").text("Result").delay(2000).queue(function(next){
+            $(this).text("Test");
+            next();
+        });
     });
 });
 
 $(document).ready(function(){
     $("#puzzle6").click(function(){
-        $("#memorise6").text("Result");
+        $("#memorise6").text("Result").delay(2000).queue(function(next){
+            $(this).text("Test");
+            next();
+        });
     });
 });
-
