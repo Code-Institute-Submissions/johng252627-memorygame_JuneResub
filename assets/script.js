40ee78ff663c3389c9f6d36d4b9eefe02e3f24b2
johng252627 <johngill252627@gmail.com>
2021-06-22 17:35:23 +0000

    Add jQuery to change color of other guesses.

--- assets/script.js
+++ assets/script.js
@@ -57,3 +57,105 @@ $(document).ready(function(){
         $("#guess1a").removeClass("guess").addClass("guessWrong");
     });
 });
+
+$(document).ready(function(){
+    $("#guess1b").click(function(){
+        $("#guess1b").removeClass("guess").addClass("guessWrong");
+    });
+});
+
+$(document).ready(function(){
+    $("#guess1c").click(function(){
+        $("#guess1c").removeClass("guess").addClass("guessRight");
+    });
+});
+
+$(document).ready(function(){
+    $("#guess2a").click(function(){
+        $("#guess2a").removeClass("guess").addClass("guessRight");
+    });
+});
+
+$(document).ready(function(){
+    $("#guess2b").click(function(){
+        $("#guess2b").removeClass("guess").addClass("guessWrong");
+    });
+});
+
+$(document).ready(function(){
+    $("#guess2c").click(function(){
+        $("#guess2c").removeClass("guess").addClass("guessWrong");
+    });
+});
+
+$(document).ready(function(){
+    $("#guess3a").click(function(){
+        $("#guess3a").removeClass("guess").addClass("guessWrong");
+    });
+});
+
+$(document).ready(function(){
+    $("#guess3b").click(function(){
+        $("#guess3b").removeClass("guess").addClass("guessWrong");
+    });
+});
+
+$(document).ready(function(){
+    $("#guess3c").click(function(){
+        $("#guess3c").removeClass("guess").addClass("guessRight");
+    });
+});
+
+$(document).ready(function(){
+    $("#guess4a").click(function(){
+        $("#guess4a").removeClass("guess").addClass("guessWrong");
+    });
+});
+
+$(document).ready(function(){
+    $("#guess4b").click(function(){
+        $("#guess4b").removeClass("guess").addClass("guessRight");
+    });
+});
+
+$(document).ready(function(){
+    $("#guess4c").click(function(){
+        $("#guess4c").removeClass("guess").addClass("guessWrong");
+    });
+});
+
+$(document).ready(function(){
+    $("#guess5a").click(function(){
+        $("#guess5a").removeClass("guess").addClass("guessRight");
+    });
+});
+
+$(document).ready(function(){
+    $("#guess5b").click(function(){
+        $("#guess5b").removeClass("guess").addClass("guessWrong");
+    });
+});
+
+$(document).ready(function(){
+    $("#guess5c").click(function(){
+        $("#guess5c").removeClass("guess").addClass("guessWrong");
+    });
+});
+
+$(document).ready(function(){
+    $("#guess6a").click(function(){
+        $("#guess6a").removeClass("guess").addClass("guessWrong");
+    });
+});
+
+$(document).ready(function(){
+    $("#guess6b").click(function(){
+        $("#guess6b").removeClass("guess").addClass("guessWrong");
+    });
+});
+
+$(document).ready(function(){
+    $("#guess6c").click(function(){
+        $("#guess6c").removeClass("guess").addClass("guessRight");
+    });
+});
